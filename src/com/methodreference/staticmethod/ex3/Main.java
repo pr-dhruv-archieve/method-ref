1b4622ef12dda75e75df5e4fe6d594769ca64bfd
pr-dhruv <pra_mahendra@hotmail.com>
2020-03-21 15:37:03 +0530

    Pre-defined functionalInterface with Overloading

--- src/com/methodreference/staticmethod/ex3/Main.java
+++ src/com/methodreference/staticmethod/ex3/Main.java
@@ -1,5 +1,26 @@
 package com.methodreference.staticmethod.ex3;
 
-public class Main {
+import java.util.function.BiFunction;
 
+public class Main {
+	public static Integer add(Integer a, Integer b) {
+		return a + b;
+	}
+	public static Float add(Integer a, Float b) {
+		return a + b;
+	}
+	public static Float add(Float a, Float b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		BiFunction<Integer, Integer, Integer> adder1 = Main::add;
+		System.out.println(adder1.apply(10, 20));
+		
+		BiFunction<Integer, Float, Float> adder2 = Main::add;
+		System.out.println(adder2.apply(10, 20.0f));
+		
+		BiFunction<Float, Float, Float> adder3 = Main::add;
+		System.out.println(adder3.apply(10.0f, 20.0f));
+	}
 }
